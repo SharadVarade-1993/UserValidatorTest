99484bbd988090ad9bee49382ee341ce25811b17
HP <varadesharad@gmail.com>
2020-04-30 20:39:55 +0530

    ADDED[usecase3]:valid Email Address.Test File

--- src/test/java/com/user/UserValidatorTest.java
+++ src/test/java/com/user/UserValidatorTest.java
@@ -5,31 +5,59 @@ import org.junit.Test;
 
 public class UserValidatorTest {
     @Test
-    public void givenFirstName_WhenProper_ShouldReturnTrue(){
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
-        boolean result=validator.validateFirstName("Sharad");
-        Assert.assertEquals(true,result);
+        boolean result = validator.validateFirstName("Sharad");
+        Assert.assertEquals(true, result);
     }
+
     @Test
-    public void givenFirstName_WhenProper_ShouldReturnFalse(){
+    public void givenFirstName_WhenProper_ShouldReturnFalse() {
         UserValidator validator = new UserValidator();
-        boolean result=validator.validateFirstName("sharad");
+        boolean result = validator.validateFirstName("sharad");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Varade");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("varade");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenEmail_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateEmailAddress("abc.xyz@cl.com.in");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail_WhenProper_ShouldReturnFalse(){
+        UserValidator validator = new UserValidator();
+        boolean result=validator.validateEmailAddress("abc.xyz@cl.com.com");
         Assert.assertEquals(false,result);
     }
 
     @Test
-    public void givenLastName_WhenProper_ShouldReturnTrue(){
+    public void givenEmailAddress_WhenProper_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
-        boolean result=validator.validateLastName("Varade");
-        Assert.assertEquals(true,result);
+        boolean result = validator.validateEmailAddress("abc@gmail.com.in");
+        Assert.assertEquals(true, result);
     }
+
     @Test
-    public  void givenLastName_WhenProper_ShouldReturnFalse(){
+    public void givenEmailAddresses_WhenProper_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
-        boolean result=validator.validateLastName("varade");
-        Assert.assertEquals(false,result);
+        boolean result = validator.validateEmailAddress("abc.123@gmail.com.in");
+        Assert.assertEquals(true, result);
+
     }
-
-
-
 }
